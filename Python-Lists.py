88c782b159525de456b660f7e9ba28631bdabc7d
maguzzz <markusm.touray@gmail.com>
2022-10-06 07:19:18 +0200

    First list and count of toppings

---
+++ Python-Lists.py
@@ -0,0 +1,8 @@
+#List toppings
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+#List Prices
+prices = [2, 6, 1, 3, 2,  7, 2]
+#Counting how many "2" are in the list "prices"
+num_two_dollar_slices = prices.count(2)
+#getting lengh of list "toppings"
+print(num_two_dollar_slices)
