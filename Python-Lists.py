7cd64d5a0689aff1112c73c138371ded1f1fe7d3
maguzzz <markusm.touray@gmail.com>
2022-10-06 07:20:04 +0200

    2D List

--- Python-Lists.py
+++ Python-Lists.py
@@ -6,3 +6,17 @@ prices = [2, 6, 1, 3, 2,  7, 2]
 num_two_dollar_slices = prices.count(2)
 #getting lengh of list "toppings"
 print(num_two_dollar_slices)
+#Printing how many pizzas are in the first list
+num_pizzas = len(toppings)
+#Printing the the number of pizza toppings
+print("We sell " + str(num_pizzas) + " diffrent kinds of pizza!")
+# 2D list with pizza price and type
+pizza_and_prices = [
+  [2, "pepperoni"],
+  [6, "pineapple"],
+  [1, "cheese"],
+  [3, "sausage"],
+  [2, "olives"],
+  [7, "anchovies"],
+  [2, "mushrooms"]
+]
